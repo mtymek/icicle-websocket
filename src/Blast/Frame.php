a5c82ce170c3ddf31ba23c540014270459816cef
Mateusz Tymek <mtymek@gmail.com>
2015-06-04 18:57:07 +0200

    finish frame parser

--- src/Blast/Frame.php
+++ src/Blast/Frame.php
@@ -12,17 +12,140 @@ class Frame
     const OPCODE_PING = 9;
     const OPCODE_PONG = 10;
 
-    public $fin;
+    /**
+     * @var int
+     */
+    private $fin;
 
-    public $rsv1;
+    /**
+     * @var int
+     */
+    private $rsv1;
 
-    public $rsv2;
+    /**
+     * @var int
+     */
+    private $rsv2;
 
-    public $rsv3;
+    /**
+     * @var int
+     */
+    private $rsv3;
 
-    public $opcode;
+    /**
+     * @var int
+     */
+    private $opcode;
 
-    public $mask;
+    /**
+     * @var string
+     */
+    private $mask;
 
-    public $length;
+    /**
+     * @var int
+     */
+    private $length;
+
+    /**
+     * @var string
+     */
+    private $maskingKey;
+
+    /**
+     * @var string
+     */
+    private $data;
+
+    /**
+     * @param $fin
+     * @param $opcode
+     * @param $mask
+     * @param $length
+     * @param $maskingKey
+     * @param $data
+     */
+    public function __construct($fin, $opcode, $mask, $length, $maskingKey, $data)
+    {
+        $this->fin = $fin;
+        $this->opcode = $opcode;
+        $this->mask = $mask;
+        $this->length = $length;
+        $this->maskingKey = $maskingKey;
+        $this->data = $data;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFin()
+    {
+        return $this->fin;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRsv1()
+    {
+        return $this->rsv1;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRsv2()
+    {
+        return $this->rsv2;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRsv3()
+    {
+        return $this->rsv3;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOpcode()
+    {
+        return $this->opcode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMask()
+    {
+        return $this->mask;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLength()
+    {
+        return $this->length;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMaskingKey()
+    {
+        return $this->maskingKey;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+
 }
--- src/Blast/FrameParser.php
+++ src/Blast/FrameParser.php
@@ -9,30 +9,35 @@ class FrameParser
 {
     public function readFrame(ClientInterface $client)
     {
-        $frame = new Frame();
         $byte1 = ord((yield $client->read(1)));
         $byte2 = ord((yield $client->read(1)));
 
-        $frame->fin    = ($byte1 & 0b10000000) >> 7;
-        $frame->rsv1   = ($byte1 & 0b01000000) >> 6;
-        $frame->rsv2   = ($byte1 & 0b00100000) >> 5;
-        $frame->rsv3   = ($byte1 & 0b00010000) >> 4;
-        $frame->opcode = ($byte1 & 0b00001111);
-        $frame->mask   = ($byte2 & 0b10000000) >> 7;
+        $fin    = ($byte1 & 0b10000000) >> 7;
+        $rsv1   = ($byte1 & 0b01000000) >> 6;
+        $rsv2   = ($byte1 & 0b00100000) >> 5;
+        $rsv3   = ($byte1 & 0b00010000) >> 4;
+        $opcode = ($byte1 & 0b00001111);
+        $mask   = ($byte2 & 0b10000000) >> 7;
 
         $length = ($byte2 & 0b01111111);
         if ($length == 126) {
             $bytes = (yield $client->read(2));
-            $frame->length = unpack('n', $bytes)[1];
-            echo 'zzz';
+            $length = unpack('n', $bytes)[1];
         } elseif ($length == 127) {
             $bytes1 = unpack('N', (yield $client->read(4)))[1];
             $bytes2 = unpack('N', (yield $client->read(4)))[1];
-            $frame->length = $bytes1 * (2 >> 32) + $bytes2;
-        } else {
-            $frame->length = $length;
+            $length = $bytes1 * (2 >> 32) + $bytes2;
         }
 
-        yield $frame;
+        $maskingKey = (yield $client->read(4));
+
+        $buffer = '';
+        for ($i = 0; $i < $length; $i++) {
+            $byte = (yield $client->read(1));
+            $buffer .= $byte ^ $maskingKey[$i % 4];
+        }
+        $data = $buffer;
+
+        yield new Frame($fin, $opcode, $mask, $length, $maskingKey, $data);
     }
 }
